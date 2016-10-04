856fa9248813b183364f6cc010f159810490d630
Geddy Schellevis <geddyschellevis@gmail.com>
2016-10-04 17:59:30 +0200

    initial

--- webCrypto.py
+++ webCrypto.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask ,jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello Wolf of CryptoStreet!'
+def index():
+    return 'Flask is running!'
+
+
+@app.route('/data')
+def names():
+    data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
+
