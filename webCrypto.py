4f98d9159974b2c49241ecd5b752e7e245fbe87d
Geddy Schellevis <geddyschellevis@gmail.com>
2017-01-04 23:51:06 +0100

    add host

--- webCrypto.py
+++ webCrypto.py
@@ -252,5 +252,5 @@ if __name__ == '__main__':
     # app.config["SECRET_KEY"] = "WOLFOFWALLSTREET"
     firebase = pyrebase.initialize_app(config)
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0' , port='8000')
 
